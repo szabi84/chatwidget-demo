3acf8965f0e0af268aa50f263b4865a4b647c235
tamas gurbacs <tamas.gurbacs@cyara.com>
2025-01-23 10:40:26 +0100

    chatwidget I improved the tab navigation on the message bot

--- chat-widget.js
+++ chat-widget.js
@@ -40,6 +40,10 @@
     cursor: pointer;
     border: none;
   }
+  #chat-bubble:focus {
+    outline: none;
+    box-shadow: inset 0 0 0 .21429rem rgba(255,255,255,0.4);
+  }
   .chat-icon{
     width: 1.2rem;
   }
@@ -79,24 +83,30 @@
     color: #ffffff;
   }
   #close-popup {
-    border-style: none;
-    color: gray;
     background-color: transparent;
     cursor: pointer;
+    border: 2px solid transparent;
+    padding: 2px 2px 0px 6px
   }
+  #close-popup:focus {
+    border: 2px solid #2f3d5470;
+    border-radius: 6px;
+  }  
   .close-icon {
     width: 1.2rem;
   }
   #fullscreen-popup {
-    border-style: none;
-    color: gray;
     background-color: transparent;
     cursor: pointer;
+    border: 2px solid transparent;
+    padding: 5px 5px 0px 5px;
   }
+  #fullscreen-popup:focus {
+    border: 2px solid #2f3d5470;
+    border-radius: 6px;
+  }    
   .fullscreen-icon {
-    width: 1.2rem;
-    position: relative;
-    top: -3px;
+    width: 1rem;
   }
   .fullscreen-icon-hidden {
     display: none;
@@ -133,11 +143,18 @@
     font-size: 14px;
     line-height: 16px;
     box-sizing: border-box;
-  }
-  #chat-input:focus {
     outline: none;
+  }
+  textarea, input {
+    outline: none;
+  }  
+  textarea:focus-visible, input:focus-visible {
+    outline: none;
+  }    
+  #chat-input:focus {
     min-height: 42px;
     height: auto;
+    outline: none;
   }
   #chat-submit {
     padding: 0.4rem 0.42rem 0.2rem 0.4rem;
@@ -147,7 +164,11 @@
     cursor: pointer;
     float: right;
     border: none;
+    border: 2px solid transparent;
   }
+  #chat-submit:focus {
+    border: 2px solid #ffffff70;
+  }     
   .user-message-container {
     display: flex;
     justify-content: flex-end;
@@ -263,7 +284,7 @@
           </svg> 
         </button>
         <button id="close-popup" aria-label="Close chat widget">
-          <svg xmlns="http://www.w3.org/2000/svg" class="close-icon" viewBox="0 0 384 512">
+          <svg xmlns="http://www.w3.org/2000/svg" class="close-icon" viewBox="0 0 512 512">
             <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/>
           </svg>
         </button>
@@ -343,6 +364,7 @@
     chatInput.value = ''
     onUserRequest(message)
     resetTextarea()
+    chatInput.focus()
   })
 
   chatInput.addEventListener('input', function () {
@@ -371,7 +393,7 @@
 
   chatBubble.addEventListener('click', function () {
     togglePopup()
-
+    chatInput.focus()
   })
 
   closePopup.addEventListener('click', function () {
@@ -399,6 +421,31 @@
       chatPopup.classList.remove('visible')
     }
   }
+
+  document.getElementById('chatPopup').addEventListener('keydown', (event) => {
+    const focusableElements = 'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
+    const modal = document.getElementById('chatPopup')
+    const focusableContent = modal.querySelectorAll(focusableElements)
+    const firstElement = focusableContent[0]
+    const lastElement = focusableContent[focusableContent.length - 1]
+  
+    // Ha a Tab billentyűt nyomja a felhasználó
+    if (event.key === 'Tab') {
+      if (event.shiftKey) {
+        // Shift + Tab: Ellenőrizd, hogy az első elemen van-e a fókusz
+        if (document.activeElement === firstElement) {
+          event.preventDefault()
+          lastElement.focus() // Ugrás az utolsó fókuszálható elemre
+        }
+      } else {
+        // Tab: Ellenőrizd, hogy az utolsó elemen van-e a fókusz
+        if (document.activeElement === lastElement) {
+          event.preventDefault()
+          firstElement.focus() // Ugrás az első fókuszálható elemre
+        }
+      }
+    }
+  });
 
   function onUserRequest(message) {
     console.log('User request:', message)
